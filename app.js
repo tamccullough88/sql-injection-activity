0768e5a29df00c6b011034309d309f70f33aa142
tamccullough88 <tamccullough88@gmail.com>
2024-01-31 20:32:17 -0500

    changed username, password, and title

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json())
 const db = new sqlite3.Database(':memory:');
 db.serialize(function () {
     db.run("CREATE TABLE user (username TEXT, password TEXT, title TEXT)");
-    db.run("INSERT INTO user VALUES ('privilegedUser', 'privilegedUser1', 'Administrator')");
+    db.run("INSERT INTO user VALUES ('myTestUserr', 'notARealPassword', 'Soon To Be Hacked User')");
 });
 
 
